872b197b96284dd0cad51c3ad52e4ec054b42327
psantodomingo10 <146854133+psantodomingo10@users.noreply.github.com>
2023-10-03 17:55:34 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -14,3 +14,20 @@ final <- zima * duccio
 final
 
 final^2
+
+# array
+Sophi <- c (10,20, 30, 50, 70) #microplastics
+
+paula <- c (100,500,600,1000,2000) #people
+
+plot (paula,Sophi)
+
+plot (paula,Sophi, xlab="number of people", ylab="microplastics")
+
+plot (paula,Sophi, xlab="number of people", ylab="microplastics",pch=19)
+
+plot (paula,Sophi, xlab="number of people", ylab="microplastics",pch=19,cex=2)
+
+plot (paula,Sophi, xlab="number of people", ylab="microplastics",pch=19,cex=1.5,col="grey")
+
+
