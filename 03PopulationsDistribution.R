ace77db495e4963e0ca218a3133b029c803da18c
psantodomingo10 <146854133+psantodomingo10@users.noreply.github.com>
2023-10-12 17:45:18 +0200

    Create 03PopulationsDistribution.R

---
+++ 03PopulationsDistribution.R
@@ -0,0 +1,53 @@
+#Why population disperce over landscape in a certain manner?
+
+library(sdm)
+library(rgdal)
+library(terra)
+file <- system.file("external/species.shp", package="sdm")
+rana <- vect(file)
+rana
+plot(rana)
+rana$Occurrence
+plot(rana[rana$Occurrence == 1,],col='black',pch=16)
+points(rana[rana$Occurrence == 0,],col='red',pch=16)
+presence <- rana[rana$Occurrence == 1]
+presence
+plot(presence,col="black")
+abse <- rana[rana$Occurrence == 0,]
+plot(abse,col="red")
+
+par(mfrow=c(1,2))
+plot(presence,col="black")
+plot(abse,col="red")
+
+plot(presence,col="black")
+
+
+path <- system.file("external", package="sdm")
+path
+lst <- list.files(path=path,pattern='asc$',full.names = T)
+lst
+
+elev <- rast("C:/Users/santo/AppData/Local/R/win-library/4.3/sdm/external/elevation.asc")
+prec <- rast("C:/Users/santo/AppData/Local/R/win-library/4.3/sdm/external/precipitation.asc")
+temp <- rast("C:/Users/santo/AppData/Local/R/win-library/4.3/sdm/external/temperature.asc")
+vege <- rast("C:/Users/santo/AppData/Local/R/win-library/4.3/sdm/external/vegetation.asc")
+
+preds <- c(elev, prec, temp, vege)
+cl <- colorRampPalette(c('red','orange','yellow','green')) (100)
+plot(preds, col=cl) 
+
+
+par(mfrow=c(2,2))
+
+plot(preds$elevation, col=cl,main="Elevation")
+points(rana[rana$Occurrence == 1,], pch=16)
+
+plot(preds$temperature, col=cl,main="Temperature")
+points(rana[rana$Occurrence == 1,], pch=16)
+
+plot(preds$precipitation, col=cl,main="Precipitation")
+points(rana[rana$Occurrence == 1,], pch=16)
+
+plot(preds$vegetation, col=cl,main="Vegetation" )
+points(rana[rana$Occurrence == 1,], pch=16)
