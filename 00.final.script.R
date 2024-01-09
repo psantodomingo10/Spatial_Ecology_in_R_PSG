c9586179785e81ef08358091cc17e344020d0977
psantodomingo10 <146854133+psantodomingo10@users.noreply.github.com>
2024-01-09 21:23:19 +0100

    Update 00.final.script.R

--- 00.final.script.R
+++ 00.final.script.R
@@ -4,16 +4,18 @@
 
 #Summary
 
-#01 Beginning
-#02.1 Population density
-#02.2 Populations distribution
-#03 Communities Multivariate Overlaping
-#04 Remote sensing visualisation
-#05 Spectral indices
-#06 Time Series
-#07 Import Data
-#08 Copernicus Data
-#09 Classification
+# 01 Beginning
+# 02.1 Population density
+# 02.2 Populations distribution
+# 03 Communities Multivariate And Overlaping Analysis
+# 04 Remote sensing visualisation
+# 05 Spectral indices
+# 06 Time Series
+# 07 Import Data
+# 08 Copernicus Data
+# 09 Classification
+# 10 Variability
+# 11 Principal Component Analysis
 
 #-----------------------------#
 
@@ -609,5 +611,92 @@ p1+p2
 p1 <- ggplot(p, aes(x=cover, y=perc1992, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p2 <- ggplot(p, aes(x=cover, y=perc2006, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p1+p2
+
+#----------------------------#
+
+# 10 Variability
+
+# measurement of RS based variability
+
+library(imageRy)
+library(terra)
+library(viridis)
+
+im.list()
+
+sent <- im.import("sentinel.png")
+
+# band 1 = NIR
+# band 2 = red
+# band 3 = green
+
+im.plotRGB(sent, r=1, g=2, b=3)
+im.plotRGB(sent, r=2, g=1, b=3)
+
+nir <- sent[[1]]
+plot(nir)
+
+# moving window
+# focal
+sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
+plot(sd3)
+
+viridisc <- colorRampPalette(viridis(7))(255)
+plot(sd3, col=viridisc)
+
+# Exercise: calculate variability in a 7x7 pixels moving window
+sd7 <- focal(nir, matrix(1/49, 7, 7), fun=sd)
+plot(sd7, col=viridisc)
+
+# Exercise 2: plot via par(mfrow()) the 3x3 and the 7x7 standard deviation
+par(mfrow=c(1,2))
+plot(sd3, col=viridisc)
+plot(sd7, col=viridisc)
+
+# original image plus the 7x7 sd
+im.plotRGB(sent, r=2, g=1, b=3)
+plot(sd7, col=viridisc)
+
+#----------------------------#
+
+# 11 Principal Component Analysis
+
+library(imageRy)
+library(terra)
+library(viridis)
+
+im.list()
+
+sent <- im.import("sentinel.png")
+
+pairs(sent)
+
+# perform PCA on sent
+sentpc <- im.pca(sent)
+pc1 <- sentpc$PC1
+
+viridisc <- colorRampPalette(viridis(7))(255)
+plot(pc1, col=viridisc)
+
+# calculating standard deviation ontop of pc1
+pc1sd3 <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
+plot(pc1sd3, col=viridisc)
+
+pc1sd7 <- focal(pc1, matrix(1/49, 7, 7), fun=sd)
+plot(pc1sd7, col=viridisc)
+
+par(mfrow=c(2,3))
+im.plotRGB(sent, 2, 1, 3)
+# sd from the variability script:
+plot(sd3, col=viridisc)
+plot(sd7, col=viridisc)
+plot(pc1, col=viridisc)
+plot(pc1sd3, col=viridisc)
+plot(pc1sd7, col=viridisc)
+
+# stack all the standard deviation layers
+sdstack <- c(sd3, sd7, pc1sd3, pc1sd7)
+names(sdstack) <- c("sd3", "sd7", "pc1sd3", "pc1sd7")
+plot(sdstack, col=viridisc)
 
 #----------------------------#
